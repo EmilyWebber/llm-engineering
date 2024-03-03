2cc0e49da8ff7209643b9ce812d0584af47fc181
Emily Webber <EmilyWebber@users.noreply.github.com>
2024-03-03 18:45:36 -0500

    Add files via upload

---
+++ integration/integration.py
@@ -0,0 +1,93 @@
+
+def run_app():
+	print ('Running app server')
+	return
+
+
+def run_llm():
+	print ('Running llm server')
+	return
+
+
+def test_llm_and_app():
+
+	print ('Testing LLM and app integration')
+
+	return
+
+
+def run_guard():
+
+	print ('Running LLM guard')
+
+	return
+
+
+def test_guard():
+
+	print ('Testing LLM guard')
+
+	return
+
+def run_bucket():
+
+	print ('Pointing to the S3 bucket')
+
+	return
+
+
+def run_vectors():
+
+	print ('Setting up the vector store')
+
+	return
+
+
+def test_vectors():
+
+	print ('Testing the vector store')
+
+	return
+
+def run_agents():
+
+	print ('Running the agent creation')
+
+	return
+
+def test_agents():
+
+	print ('Testing the agents')
+
+	return
+
+def main():
+
+	run_app()
+
+	run_llm()
+
+	test_llm_and_app()
+
+	run_guard()
+
+	test_guard()
+
+	run_bucket()
+
+	run_vectors()
+
+	test_vectors()
+
+	run_agents()
+
+
+
+
+if __name__ == "__main__":
+
+	print ('Running integration tests')
+
+	main()
+
+	print ('Integration tests complete')
